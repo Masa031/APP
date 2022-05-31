700b529a0c958df97bb1592a610ab9a3919456cd
Marija Šakić <sakicmarija35@gmail.com>
2022-05-31 21:17:34 +0300

    Update kozmeticki_salon.sql

--- kozmeticki_salon.sql
+++ kozmeticki_salon.sql
@@ -19,7 +19,6 @@ create table korisnik(
     prezime varchar(50) not null,
     email varchar(100),
     lozinka varchar(50),
-    usluga int not null,
     termin datetime 
 );
 
@@ -35,7 +34,7 @@ create table usluga(
     sifra int not null primary key auto_increment,
     naziv varchar(50) not null,
     trajanje int,
-    cijena decimal(18,3),
+    cijena decimal(18,6),
     djelatnik  int not null,
     korisnik int not null,
     termin datetime
@@ -43,6 +42,19 @@ create table usluga(
 
 alter table termin add foreign key (korisnik) references korisnik(sifra);
 alter table termin add foreign key (usluga) references usluga(sifra);
-alter table termin add foreign key(usluga) references usluga(sifra);
+alter table termin add foreign key(djelatnik) references djelatnik(sifra);
 alter table usluga add foreign key(korisnik) references korisnik(sifra);
 alter table djelatnik add foreign key(korisnik) references korisnik(sifra);
+
+
+select *from djelatnik;
+insert into djelatnik (sifra,ime,prezime,email,iban,korisnik)
+values (null,'Maja','Perić','pericmaja@gmail.com','HR2425000095981288512',1);
+
+select *from korisnik;
+insert into korisnik (sifra,ime,prezime,email,lozinka,termin)
+values(null,'Marina','Marijanović','marijanam@gmail.com',null,null);
+
+select *from usluga;
+insert into usluga (sifra,naziv,trajanje,cijena,djelatnik,korisnik,termin)
+values(null,'Manikura','60','150.00',3,1,,'2022-10-06 15:30:00');
